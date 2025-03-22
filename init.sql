dabc0db5f00bc048418d7234e23c5e25d4d9efa2
rauan <ommirkulr@gmail.com>
2025-03-23 00:11:24 +0500

    upd: postgres tables

--- init.sql
+++ init.sql
@@ -2,8 +2,59 @@ CREATE TYPE sex AS ENUM ('man', 'woman');
 CREATE TYPE order_status AS ENUM ('pending', 'confirmed', 'in progress', 'completed', 'cancelled');
 
 CREATE TABLE orders (
-  order_id SERIAL PRIMARY KEY,
-  customer_id TEXT NOT NULL,
-  status order_status NOT NULL DEFAULT 'pending',
-  order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+    order_id SERIAL PRIMARY KEY,
+    customer_id INT NOT NULL,
+    status VARCHAR(50) NOT NULL DEFAULT 'pending',
+    order_date TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    total INTEGER NOT NULL
+);
+
+CREATE TABLE order_items (
+    order_items_id SERIAL PRIMARY KEY,
+    order_id INT REFERENCES orders(order_id) ON DELETE CASCADE,
+    menu_item_id INT REFERENCES menu_item(menu_item_id) ON DELETE CASCADE,
+    quantity INT NOT NULL
+);
+
+CREATE TABLE menu_item (
+    menu_item_id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT,
+    price INT NOT NULL
+);
+
+CREATE TABLE menu_item_ingredients (
+    menu_item_ingredients_id SERIAL PRIMARY KEY,
+    menu_item_id INT REFERENCES menu_item(menu_item_id) ON DELETE CASCADE,
+    inventory_item_id INT REFERENCES inventory_item(inventory_item_id) ON DELETE CASCADE,
+    quantity INT NOT NULL
+);
+
+CREATE TABLE inventory_item (
+    inventory_item_id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    quantity INT NOT NULL,
+    unit TEXT NOT NULL
+);
+
+CREATE TABLE inventory_transactions (
+    transaction_id SERIAL PRIMARY KEY,
+    inventory_item_id INT REFERENCES inventory_item(inventory_item_id) ON DELETE CASCADE,
+    quantity_change INT NOT NULL,
+    transaction_date TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE order_status_history (
+    status_history_id SERIAL PRIMARY KEY,
+    order_id INT REFERENCES orders(order_id) ON DELETE CASCADE,
+    status VARCHAR(50) NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE price_history (
+    price_history_id SERIAL PRIMARY KEY,
+    menu_item_id INT REFERENCES menu_item(menu_item_id) ON DELETE CASCADE,
+    old_price INT NOT NULL,
+    new_price INT NOT NULL,
+    changed_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
