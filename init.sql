6acecf4f8214948bcbb96caa6f3d883407248da2
rauan <ommirkulr@gmail.com>
2025-03-22 06:02:32 +0500

    Craeted new table in sql

--- init.sql
+++ init.sql
@@ -1,1 +1,7 @@
-CREATE TYPE sex AS ENUM ('man', 'woman')
+CREATE TYPE sex AS ENUM ('man', 'woman');
+
+CREATE TABLE orders(
+  order_id SERIAL PRIMARY KEY,
+  customer_id VARCHAR(255) NOT NULL,
+  order_date DATE NOT NULL
+);
