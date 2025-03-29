e6e00fce2a3a2d3d9437d666d14cd8e6323dc7fe
urystem <urystem01.kz@gmail.com>
2025-03-29 08:48:25 +0500

    cannot link go and psql

--- cmd/main.go
+++ cmd/main.go
@@ -4,26 +4,26 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"time"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 func main() {
-	db, err := sql.Open("pgx", "postgres://latte:latte@localhost:5432/frappuccino")
+	_, err := sql.Open("pgx", "postgres://latte:latte@db:5432/frappuccino")
 	if err != nil {
 		log.Fatalf("%v", err)
+		fmt.Println("qyzyq")
 	}
-	for {
-		var now string
-		err = db.QueryRow("SELECT NOW()").Scan(&now)
-		if err != nil {
-			log.Fatalf("Ошибка запроса: %v", err)
-		}
-		// fmt.Println("hello")
+	// for {
+	// 	var now string
+	// 	err = db.QueryRow("SELECT NOW()").Scan(&now)
+	// 	if err != nil {
+	// 		log.Fatalf("Ошибка запроса: %v", err)
+	// 	}
+	// 	// fmt.Println("hello")
 
-		fmt.Println("⏰ Время в БД:", now)
-		time.Sleep(time.Second * 5)
-	}
+	// 	fmt.Println("⏰ Время в БД:", now)
+	// 	time.Sleep(time.Second * 5)
+	// }
 	// log.Fatalln(http.ListenAndServe(":"+"8080", router.Allrouter(db)))
 }
